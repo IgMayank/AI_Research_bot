8e6a111bcd1553107e298475a751c6e6c104116f
IgMayank <mkdogra1981@gmail.com>
2026-04-11 22:57:08 +0530

    changes in memory

--- apps/data_ingestion.py
+++ apps/data_ingestion.py
@@ -1,8 +1,8 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
 from langchain_community.vectorstores import InMemoryVectorStore
+import store
 import os
 
 
@@ -12,33 +12,22 @@ def process_document(path):
     docs = loader.load()
 
     # split
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=250)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=250)
     splitted_chunks = splitter.split_documents(documents=docs)
-    for i ,chunk in enumerate(splitted_chunks):
+    for i, chunk in enumerate(splitted_chunks):
         chunk.metadata["chunk_id"] = i
-        
         if 'source' in chunk.metadata:
             chunk.metadata["source"] = os.path.basename(chunk.metadata["source"])
 
-    # embeddings and vectordb
-
+    # embeddings
     embeddings = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-base-en-v1.5",
-    model_kwargs={"device": "cpu"},
-    encode_kwargs={
-        "normalize_embeddings": True
-    }
-)
-    vector_store = InMemoryVectorStore.from_documents(
+        model_name="BAAI/bge-base-en-v1.5",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
+    )
+
+  
+    store.vector_store = InMemoryVectorStore.from_documents(
         documents=splitted_chunks,
         embedding=embeddings,
-        persist_directory="./vector_store_"
     )
-    # vector_store.persist()
-
-
-
-
-
-
-    
--- apps/data_retrieval.py
+++ apps/data_retrieval.py
@@ -1,61 +1,9 @@
-from langchain_chroma import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
-# from query_handling import query
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-base-en-v1.5",
-    model_kwargs={"device": "cpu"},
-    encode_kwargs={
-        "normalize_embeddings": True
-    })
-vector_store = Chroma(
-        persist_directory="./vector_store_",
-        embedding_function=embeddings,)
+import store
 
 
-def retrieve_data(query:str , k:int=4):
+def retrieve_data(query: str, k: int = 4):
+    if store.vector_store is None:
+        raise ValueError("Vector store is empty. Please upload documents first.")
     
-    docs = vector_store.similarity_search(query , k=k)
-
+    docs = store.vector_store.similarity_search(query, k=k)
     return docs
-
-
-
-
-
-
-
-
-
-
-# @tool
-# def retrieve_data(query):
-#     """ Use this tool when u need additional information from documents 
-#     """
-#     # embeddings = HuggingFaceEmbeddings(
-#     #     model_name="all-MiniLM-L6-v2"
-#     # )
-
-#     embeddings = HuggingFaceEmbeddings(
-#     model_name="BAAI/bge-base-en-v1.5",
-#     model_kwargs={"device": "cpu"},
-#     encode_kwargs={
-#         "normalize_embeddings": True
-#     }
-# )
-#     # load exixsting db
-#     vector_store = Chroma(
-#         persist_directory="./vector_store_",
-#         embedding_function=embeddings,
-#     )
-#     # retrival of top k
-#     docs = vector_store.similarity_search(query , k=4)
-
-#     return docs
-
-# results = retrieve_data(query)
-
-
-
-
-        
